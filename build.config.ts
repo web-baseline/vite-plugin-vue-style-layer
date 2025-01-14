4f4a389d1b84739294eb9cdc993c70a12fc0a7fe
Tm-SunnyDay <1581272410@qq.com>
2025-01-14 16:47:12 +0800

    chore: 发布版本0.2.0

--- build.config.ts
+++ build.config.ts
@@ -2,10 +2,17 @@ import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
   entries: ['src/index'],
-  externals: ['vite', 'postcss'],
+  externals: [
+    'vite',
+    'postcss',
+    '@web-baseline/postcss-wrap-up-layer',
+  ],
   clean: true,
   declaration: 'compatible',
   rollup: {
+    output: {
+      exports: 'named',
+    },
     emitCJS: true,
     inlineDependencies: true,
   },
